dbb385b9ac22e4d56ffe1904202e6ba7222163c3
tomMoral <thomas.moreau.2010@gmail.com>
2019-09-02 15:12:20 +0200

    FIX db in heroku

--- palaisien/settings.py
+++ palaisien/settings.py
@@ -119,11 +119,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
@@ -138,3 +133,7 @@ STATIC_URL = '/static/'
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+import django_heroku
+django_heroku.settings(locals())
