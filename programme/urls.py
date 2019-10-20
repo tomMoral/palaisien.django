d1d6bc4f2468d75cec804b2cf7b1e14cef824602
tomMoral <thomas.moreau.2010@gmail.com>
2019-10-20 08:01:50 +0200

    FIX favicon

--- programme/urls.py
+++ programme/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, re_path
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 from . import views
 
 urlpatterns = [
     path('<int:seminar_id>/', views.register, name='register'),
     re_path(r'^[^/]*/?$', views.index, name='index'),
-    # url(r'^CV/pyparis17.*$', 'django.contrib.staticfiles.views.serve', kwargs={
-    #     'path': 'CV/pyparis17/index.html',
-    #     'document_root': settings.STATIC_ROOT}),
+    path('favicon.ico', RedirectView.as_view(
+        url=staticfiles_storage.url('programme/images/favicon.png')))
 ]
