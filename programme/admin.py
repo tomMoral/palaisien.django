ad75bb678d0d42087a2b1095959bf0480a093208
tomMoral <thomas.moreau.2010@gmail.com>
2019-11-12 15:03:17 +0100

    ENH add fonction to list attendees for a given seminar

--- programme/admin.py
+++ programme/admin.py
@@ -1,8 +1,36 @@
 from django.contrib import admin
+from django.http import HttpResponse
 
 # Register your models here.
 from .models import Attendees, Seminar, Talk
 
-admin.site.register(Seminar)
+
+HEADER = "\t\t\t{}\nSurname\tName\tInstitution\n"
+
+
+class SeminarAdmin(admin.ModelAdmin):
+    list_display = ['date', 'place']
+    ordering = ['date']
+    actions = ['list_attendees']
+
+    def list_attendees(self, request, queryset):
+        seminar = queryset.first()
+        if seminar is None:
+            return
+
+        attendees = seminar.attendees_set.iterator()
+        header = HEADER.format(seminar)
+        serialize_csv = header + "\n".join([
+            '\t'.join([a.surname, a.name, a.institution])
+            for a in attendees])
+
+        response = HttpResponse(serialize_csv, content_type='text/csv')
+
+        return response
+
+    list_attendees.short_description = "List attendees"
+
+
+admin.site.register(Seminar, SeminarAdmin)
 admin.site.register(Talk)
 admin.site.register(Attendees)
