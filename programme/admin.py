6cf7a4e5e260796b03b046370a33ad378f21bc08
tomMoral <thomas.moreau.2010@gmail.com>
2020-01-02 11:47:01 +0100

    ENH add button to create announcement emails

--- programme/admin.py
+++ programme/admin.py
@@ -1,5 +1,8 @@
+import urllib
 from django.contrib import admin
 from django.http import HttpResponse
+from django.utils.html import format_html
+
 
 # Register your models here.
 from .models import Attendees, Seminar, Talk
@@ -8,8 +11,22 @@ from .models import Attendees, Seminar, Talk
 HEADER = "\t\t\t{}\nSurname\tName\tInstitution\n"
 
 
+EMAIL_BODY_TEMPLATE = """
+Dear all,
+
+
+We hope to see you soon,
+Best regards,
+Victor-Emmanuel & Thomas for the "Séminaire Palaisien"
+
+============================================================
+{title_and_abstracts}
+============================================================
+"""
+
+
 class SeminarAdmin(admin.ModelAdmin):
-    list_display = ['date', 'place', 'number_attendees']
+    list_display = ['date', 'place', 'number_attendees', 'create_email']
     ordering = ['date']
     actions = ['list_attendees']
 
@@ -31,6 +48,30 @@ class SeminarAdmin(admin.ModelAdmin):
 
         return response
 
+    def create_email(self, obj):
+        to = "palaisien@inria.fr"
+        subject = f"[Séminaire Palaisien] {obj.date:%A %d/%m/%y} - {obj.place}"
+        subject = urllib.parse.quote(subject)
+
+        talks = obj.talk_set.iterator()
+        title_and_abstracts = []
+        time_slots = ["16h-16h40", "16h40-17h20"] + ["00h-00h"] * 10
+        for talk, slot in zip(talks, time_slots):
+            speaker = f"[{talk.speaker}]({talk.site})"
+            abstract = f"Abstract: {talk.abstract}"
+            description = f"{slot} - {speaker} - {talk.title}\n\n{abstract}\n"
+            title_and_abstracts.append(description)
+
+        title_and_abstracts = ("-"*80 + '\n').join(title_and_abstracts)
+        body = urllib.parse.quote(EMAIL_BODY_TEMPLATE.format(
+            title_and_abstracts=title_and_abstracts))
+
+        href = f"mailto:{to}?subject={subject}&body={body}"
+
+        return format_html('<a href="{href}">send announcement</a>',
+                           href=href)
+
+    create_email.short_description = "Announcement email"
     list_attendees.short_description = "List attendees"
 
 
